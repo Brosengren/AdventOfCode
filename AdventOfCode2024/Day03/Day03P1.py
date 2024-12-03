68c77c467c6a8a648d0f81dffd315711dc013b49
Braden <bradenrosengren@gmail.com>
2024-12-03 00:45:54 -0600

    Day 3

--- AdventOfCode2024/Day03/Day03P1.py
+++ AdventOfCode2024/Day03/Day03P1.py
@@ -1,1 +1,17 @@
+from pathlib import Path
+filename = Path(__file__).parent.__str__() + "/Day03IN.txt"
+with open(filename) as f:
+    raw_input = [x for x in f]
 
+
+import re
+mul_list = []
+for line in raw_input:
+    mul_list += re.findall("mul\(\d*,\d*\)", line)
+total = 0
+for mul in mul_list:
+    nums = re.findall("\d*", mul)
+    nums = list(map(int, [x for x in nums if x]))
+    total += nums[0] * nums[1]
+
+print(total)
--- AdventOfCode2024/Day03/Day03P2.py
+++ AdventOfCode2024/Day03/Day03P2.py
@@ -1,1 +1,27 @@
+from pathlib import Path
+filename = Path(__file__).parent.__str__() + "/Day03IN.txt"
+with open(filename) as f:
+    raw_input = [x for x in f]
 
+
+import re
+mul_list = []
+for line in raw_input:
+    mul_list += re.findall("mul\(\d*,\d*\)|don't\(\)|do\(\)", line)
+
+total = 0
+doing = True
+for mul in mul_list:
+    print(mul)
+    if doing:
+        if mul == "don't()":
+            doing = False
+        elif mul != "do()":
+            nums = re.findall("\d*", mul)
+            nums = list(map(int, [x for x in nums if x]))
+            total += nums[0] * nums[1]
+    if not doing:
+        if mul == "do()":
+            doing = True
+
+print(total)
