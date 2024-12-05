857efd53305e17bca3f193fc0fb2dc5d90fe19cf
Braden <bradenrosengren@gmail.com>
2024-12-05 00:48:08 -0600

    day 4

--- AdventOfCode2024/Day04/Day04P1.py
+++ AdventOfCode2024/Day04/Day04P1.py
@@ -1,1 +1,47 @@
+from pathlib import Path
+filename = Path(__file__).parent.__str__() + "/Day04IN.txt"
+with open(filename) as f:
+    raw_input = [x for x in f]
 
+x = 'X'
+m = 'M'
+a = 'A'
+s = 'S'
+count = 0
+
+for i in range(len(raw_input)):
+    for j in range(len(raw_input[i])):
+        if raw_input[i][j] == x:
+            try:
+                if raw_input[i-1][j-1] == m and raw_input[i-2][j-2] == a and raw_input[i-3][j-3] == s:
+                    count += 1
+            except:pass
+            try:
+                if raw_input[i-1][j] == m and raw_input[i-2][j] == a and raw_input[i-3][j] == s:
+                    count += 1
+            except:pass
+            try:
+                if raw_input[i-1][j+1] == m and raw_input[i-2][j+2] == a and raw_input[i-3][j+3] == s:
+                    count += 1
+            except:pass
+            try:
+                if raw_input[i][j-1] == m and raw_input[i][j-2] == a and raw_input[i][j-3] == s:
+                    count += 1
+            except:pass
+            try:
+                if raw_input[i][j+1] == m and raw_input[i][j+2] == a and raw_input[i][j+3] == s:
+                    count += 1
+            except:pass
+            try:
+                if raw_input[i+1][j-1] == m and raw_input[i+2][j-2] == a and raw_input[i+3][j-3] == s:
+                    count += 1
+            except:pass
+            try:
+                if raw_input[i+1][j] == m and raw_input[i+2][j] == a and raw_input[i+3][j] == s:
+                    count += 1
+            except:pass
+            try:
+                if raw_input[i+1][j+1] == m and raw_input[i+2][j+2] == a and raw_input[i+3][j+3] == s:
+                    count += 1
+            except:pass
+print(count)
--- AdventOfCode2024/Day04/Day04P2.py
+++ AdventOfCode2024/Day04/Day04P2.py
@@ -1,1 +1,28 @@
+from pathlib import Path
+filename = Path(__file__).parent.__str__() + "/Day04IN.txt"
+with open(filename) as f:
+    puz = [x.strip() for x in f]
 
+m = 'M'
+a = 'A'
+s = 'S'
+count = 0
+
+for i in range(len(puz)):
+    for j in range(len(puz[i])):
+        if i == 0 or i == len(puz)-1 or j == 0 or j == len(puz[i])-1:
+            continue
+        if puz[i][j] == a:
+            ul = puz[i-1][j-1]
+            ur = puz[i-1][j+1]
+            ll = puz[i+1][j-1]
+            lr = puz[i+1][j+1]
+            if (
+               (ul == m and ur == m and lr == s and ll == s)
+            or (ul == s and ur == m and lr == m and ll == s)
+            or (ul == s and ur == s and lr == m and ll == m)
+            or (ul == m and ur == s and lr == s and ll == m)
+            ):
+                count += 1
+         
+print(count)
