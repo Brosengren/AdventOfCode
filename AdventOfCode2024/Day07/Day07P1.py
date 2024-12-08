c7db29f9371021dcbe8fbe907522ee48bc7b2093
Braden <bradenrosengren@gmail.com>
2024-12-08 01:14:56 -0600

    day 7

--- AdventOfCode2024/Day07/Day07P1.py
+++ AdventOfCode2024/Day07/Day07P1.py
@@ -1,1 +1,36 @@
+from pathlib import Path
+filename = Path(__file__).parent.__str__() + "/Day07IN.txt"
+with open(filename) as f:
+    raw_input = [x.split() for x in f]
+
+def operate(total=0, index=0) -> bool:
+    global answer
+    global elements
+
+    if index == len(elements):
+        return total == answer
+
+    tot = total + elements[index]
+    if operate(tot, index +1):
+        return True
+    
+    tot = total * elements[index]
+    if operate(tot, index+1):
+        return True
+    
+    return False
+
+
+count = 0
+count_total = 0
+for line in raw_input:
+    answer = int(line[0].removesuffix(':'))
+    elements = list(map(int, line[1:]))
+
+    if operate():
+        count += 1
+        count_total += answer
+
+print(count)
+print(count_total)
 
